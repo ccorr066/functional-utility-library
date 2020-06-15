befb0bd6498b8077f8ef8f9b1816a923fc5f0eff
Christian Corrales <ccorr066@fiu.edu>
2020-06-15 13:58:56 -0700

    added names to functions and examples from fcc

--- functions.js
+++ functions.js
@@ -1,1 +1,263 @@
-// Your functions go here!
+// freecodecamp reference
+
+function headsOrTails(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+// freecodecamp reference
+
+function headsOrTails(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+// freecodecamp reference
+
+function headsOrTails(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+// freecodecamp reference
+
+function headsOrTails(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+// freecodecamp reference
+
+function headsOrTails(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+// freecodecamp reference
+
+function ComplexHigherOrder(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+// freecodecamp reference
+
+// The global variable
+var watchList = [
+   {
+      Title: "Inception",
+      Year: "2010",
+      Rated: "PG-13",
+      Released: "16 Jul 2010",
+      Runtime: "148 min",
+      Genre: "Action, Adventure, Crime",
+      Director: "Christopher Nolan",
+      Writer: "Christopher Nolan",
+      Actors: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+      Plot:
+         "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+      Language: "English, Japanese, French",
+      Country: "USA, UK",
+      Awards: "Won 4 Oscars. Another 143 wins & 198 nominations.",
+      Poster:
+         "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+      Metascore: "74",
+      imdbRating: "8.8",
+      imdbVotes: "1,446,708",
+      imdbID: "tt1375666",
+      Type: "movie",
+      Response: "True",
+   },
+   {
+      Title: "Interstellar",
+      Year: "2014",
+      Rated: "PG-13",
+      Released: "07 Nov 2014",
+      Runtime: "169 min",
+      Genre: "Adventure, Drama, Sci-Fi",
+      Director: "Christopher Nolan",
+      Writer: "Jonathan Nolan, Christopher Nolan",
+      Actors: "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+      Plot:
+         "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      Language: "English",
+      Country: "USA, UK",
+      Awards: "Won 1 Oscar. Another 39 wins & 132 nominations.",
+      Poster:
+         "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+      Metascore: "74",
+      imdbRating: "8.6",
+      imdbVotes: "910,366",
+      imdbID: "tt0816692",
+      Type: "movie",
+      Response: "True",
+   },
+   {
+      Title: "The Dark Knight",
+      Year: "2008",
+      Rated: "PG-13",
+      Released: "18 Jul 2008",
+      Runtime: "152 min",
+      Genre: "Action, Adventure, Crime",
+      Director: "Christopher Nolan",
+      Writer:
+         "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+      Actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+      Plot:
+         "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+      Language: "English, Mandarin",
+      Country: "USA, UK",
+      Awards: "Won 2 Oscars. Another 146 wins & 142 nominations.",
+      Poster:
+         "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+      Metascore: "82",
+      imdbRating: "9.0",
+      imdbVotes: "1,652,832",
+      imdbID: "tt0468569",
+      Type: "movie",
+      Response: "True",
+   },
+   {
+      Title: "Batman Begins",
+      Year: "2005",
+      Rated: "PG-13",
+      Released: "15 Jun 2005",
+      Runtime: "140 min",
+      Genre: "Action, Adventure",
+      Director: "Christopher Nolan",
+      Writer:
+         "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+      Actors: "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+      Plot:
+         "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+      Language: "English, Urdu, Mandarin",
+      Country: "USA, UK",
+      Awards: "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+      Poster:
+         "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+      Metascore: "70",
+      imdbRating: "8.3",
+      imdbVotes: "972,584",
+      imdbID: "tt0372784",
+      Type: "movie",
+      Response: "True",
+   },
+   {
+      Title: "Avatar",
+      Year: "2009",
+      Rated: "PG-13",
+      Released: "18 Dec 2009",
+      Runtime: "162 min",
+      Genre: "Action, Adventure, Fantasy",
+      Director: "James Cameron",
+      Writer: "James Cameron",
+      Actors: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+      Plot:
+         "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+      Language: "English, Spanish",
+      Country: "USA, UK",
+      Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
+      Poster:
+         "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+      Metascore: "83",
+      imdbRating: "7.9",
+      imdbVotes: "876,575",
+      imdbID: "tt0499549",
+      Type: "movie",
+      Response: "True",
+   },
+];
+function getRating(watchList) {
+   var count = 0;
+   var averageRating =
+      watchList.reduce((sum, movie) => {
+         if (movie.Director == "Christopher Nolan") {
+            count += 1;
+            return sum + parseFloat(movie.imdbRating);
+         }
+         return sum;
+      }, 0) / count;
+
+   return averageRating;
+}
+console.log(getRating(watchList));
+
+function oneAnswerReduce(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+// freecodecamp reference
+function nonMutatingPush(original, newItem) {
+   return original.concat(newItem);
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingPush(first, second);
+
+function noPushConcat(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+// freecodecamp reference
+function nonMutatingConcat(original, attach) {
+   return original.concat(attach);
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingConcat(first, second);
+
+function keepTogetherConcat(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+
+// freecodecamp reference
+function nonMutatingSplice(cities) {
+   return cities.slice(0, 3);
+}
+var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
+
+function noChangeSplice(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+// freecodecamp reference
+function sliceArray(anim, beginSlice, endSlice) {
+   return anim.slice(beginSlice, endSlice);
+}
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
+
+function sliceList(input1) {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
+
+// freecodecamp reference
+let s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function (callback) {
+   let newArray = [];
+   s.forEach((elem) => {
+      if (callback(elem) === true) {
+         newArray.push(elem);
+      }
+   });
+   return newArray;
+};
+
+let new_s = s.myFilter(function (item) {
+   return item % 2 === 1;
+});
+
+function filterIt() {
+   // A1: variable counter will determine the tally between heads or tails
+   // A2: the conditional statment to determine whether if the user picks heads or tails
+   // R: will return either heads or tails and keep tally of which is winnning
+}
